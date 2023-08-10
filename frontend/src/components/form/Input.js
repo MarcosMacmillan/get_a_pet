c8c2ee8c2f69f0b8bdd1ee749d1e056e953b1080
Marcos Macmillan <marcosfalar@gmail.com>
2023-08-10 10:05:47 -0300

    feat(mergeRepositories): Mergeando Frontend e Backend (monorepo)

---
+++ frontend/src/components/form/Input.js
@@ -0,0 +1,28 @@
+import syles from "./Input.module.css";
+
+function Input({
+  type,
+  text,
+  name,
+  placeholder,
+  handleOnChange,
+  value,
+  multiple,
+}) {
+  return (
+    <div className={syles.form_control}>
+      <label htmlFor={name}>{text}</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={handleOnChange}
+        value={value}
+        {...(multiple ? { multiple } : "")}
+      />
+    </div>
+  );
+}
+
+export default Input;
